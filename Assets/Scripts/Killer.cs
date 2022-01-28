6e88626170265f59a607331035c9d9ee5c5b91ca
Christoper Manzano <58918297+chriss1245@users.noreply.github.com>
2022-01-29 00:26:26 +0100

    rrtew

--- Assets/Scripts/Killer.cs
+++ Assets/Scripts/Killer.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Killer : MonoBehaviour
 {
+
+    Transform transform;
+    Vector3 orginalPos;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        transform = GetComponent<Transform>();
+        orginalPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+       void Update()
     {
-        
+        if (transform.position[1] < -20)
+        {
+            transform.position = orginalPos;
+            rb.velocity = new Vector2(0,0);
+            //Destroy(gameObject);
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("lol");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("lol");
     }
 }
